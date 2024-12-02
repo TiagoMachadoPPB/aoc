124b049780ed99fa7dc6770bb94b4abdee2e4f61
TiagoMachadoPPB <tiagomachado@blip.pt>
2024-12-02 09:31:33 +0000

    polish

--- src/main/java/com/tiago/aoc/Solver.java
+++ src/main/java/com/tiago/aoc/Solver.java
@@ -2,27 +2,38 @@ package com.tiago.aoc;
 
 import com.tiago.aoc.utils.InputReader;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
 
 public class Solver {
-    public static void solveAll() {
+
+    public static void solveAll() throws Exception{
         for (int day = 1; day <= 25; day++) {
-            try {
+
                 String className = String.format("com.tiago.aoc.days.Day%02d", day);
                 Class<?> dayClass = Class.forName(className);
                 Object instance = dayClass.getDeclaredConstructor().newInstance();
-
-                Method part1 = dayClass.getMethod("solvePart1", List.class);
-                Method part2 = dayClass.getMethod("solvePart2", List.class);
-
                 List<String> input = InputReader.readInputAsList("day" + String.format("%02d", day) + ".txt");
-                System.out.printf("Day %02d - Part 1: %s%n", day, part1.invoke(instance, input));
-                System.out.printf("Day %02d - Part 2: %s%n", day, part2.invoke(instance, input));
-            } catch (Exception e) {
-                System.out.printf("Error solving day %02d: %s%n", day, e.getMessage());
-            }
+
+                // Part 1
+                System.out.printf("Day %02d%n", day);
+                executePart(instance, input, 1);
+                executePart(instance, input, 2);
+
+
         }
+    }
+
+    private static Object executePart(Object instance, List<?> input, int part) throws Exception {
+        Method method = instance.getClass().getMethod("part" + part, List.class);
+
+        long start = System.currentTimeMillis();
+        Object invoke = method.invoke(instance, input);
+        long end = System.currentTimeMillis();
+        System.out.printf("Part %d: %2s -- Took %d ms %n", part, invoke, end - start);
+
+        return invoke;
     }
 }
 
---
+++ src/main/java/com/tiago/aoc/days/Day.java
@@ -0,0 +1,8 @@
+package com.tiago.aoc.days;
+
+import java.util.List;
+
+public interface Day {
+    public int part1(List<String> strings);
+    public int part2(List<String> strings);
+}
